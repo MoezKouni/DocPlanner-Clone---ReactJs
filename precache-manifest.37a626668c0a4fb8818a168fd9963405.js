e964672b97d80b6268c3cc67c4bc783ad8f4490a
MoezKouni <mo.elkouni@gmail.com>
2019-07-04 10:18:01 +0100

    Updates

---
+++ precache-manifest.37a626668c0a4fb8818a168fd9963405.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4e6a696de8d1554a3b35f16043dce94",
+    "url": "/DocPlanner-Clone---ReactJs/index.html"
+  },
+  {
+    "revision": "77fb18230283a96111ba",
+    "url": "/DocPlanner-Clone---ReactJs/static/css/main.3f1a6204.chunk.css"
+  },
+  {
+    "revision": "4ae26aea80190adf13b1",
+    "url": "/DocPlanner-Clone---ReactJs/static/js/2.b9a36bca.chunk.js"
+  },
+  {
+    "revision": "77fb18230283a96111ba",
+    "url": "/DocPlanner-Clone---ReactJs/static/js/main.9bfa4f7a.chunk.js"
+  },
+  {
+    "revision": "62e72050f976daf58b5f",
+    "url": "/DocPlanner-Clone---ReactJs/static/js/runtime~main.d1841817.js"
+  },
+  {
+    "revision": "c29854acac38fdc1c343e5ab45e1e054",
+    "url": "/DocPlanner-Clone---ReactJs/static/media/bg.c29854ac.png"
+  },
+  {
+    "revision": "120076c7e404fb282e53f6885f38dac7",
+    "url": "/DocPlanner-Clone---ReactJs/static/media/doctors.120076c7.png"
+  },
+  {
+    "revision": "f70458ad8dd2bffe46c6c83ce8e8a30a",
+    "url": "/DocPlanner-Clone---ReactJs/static/media/logo.f70458ad.svg"
+  },
+  {
+    "revision": "82d323de468a8ad9636c43e8de2a26ba",
+    "url": "/DocPlanner-Clone---ReactJs/static/media/patients.82d323de.png"
+  }
+]);
